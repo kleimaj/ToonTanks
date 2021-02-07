2c4597cc40be676ea7462b37a646568ee3c85475
kleimaj <jacobakleiman@gmail.com>
2021-02-06 22:08:16 -0800

    adds PawnTank constructor, connects SpringArm to Camera in game hierarchy

--- Source/ToonTanks/Pawns/PawnTank.h
+++ Source/ToonTanks/Pawns/PawnTank.h
@@ -6,12 +6,33 @@
 #include "PawnBase.h"
 #include "PawnTank.generated.h"
 
-/**
- * 
- */
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class TOONTANKS_API APawnTank : public APawnBase
 {
 	GENERATED_BODY()
 	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* SpringArm = nullptr;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components", meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* Camera = nullptr;
+
+public:
+
+	APawnTank();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 };
